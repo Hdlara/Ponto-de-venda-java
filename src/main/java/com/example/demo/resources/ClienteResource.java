1f55f8cfa6e58b49790d856d4d0250b64d255546
Henrique Dias <henriquediaslara@hotmail.com>
2020-05-29 14:59:26 -0300

    Deletar Cliente

--- src/main/java/com/example/demo/resources/ClienteResource.java
+++ src/main/java/com/example/demo/resources/ClienteResource.java
@@ -1,15 +1,18 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.entities.Cliente;
 import com.example.demo.services.ClienteService;
@@ -38,7 +41,14 @@ public class ClienteResource {
 	@PostMapping
 	public ResponseEntity<Cliente> insert(@RequestBody Cliente obj){
 		obj = service.insert(obj);
-		return ResponseEntity.ok().body(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); 
+		return ResponseEntity.created(uri).body(obj);
 
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
